7265567a3b5498bc492e230a7ace16a2ccae1ef9
Lyan_Nandyan <lyannandyan@gmail.com>
2025-06-08 17:43:44 +0700

    Refactor location permission handling: separate ensureLocationPermission method and adjust getCurrentLocation to utilize it

--- lib/core/service/location_service.dart
+++ lib/core/service/location_service.dart
@@ -1,20 +1,32 @@
 import 'package:geolocator/geolocator.dart';
 
 class LocationService {
-  Future<Position> getCurrentLocation() async {
+  Future<bool> ensureLocationPermission() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       throw Exception('Layanan lokasi tidak aktif');
     }
 
     LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+    if (permission == LocationPermission.denied ||
+        permission == LocationPermission.deniedForever) {
       permission = await Geolocator.requestPermission();
-      if (permission != LocationPermission.whileInUse && permission != LocationPermission.always) {
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
         throw Exception('Izin lokasi ditolak');
       }
     }
 
-    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    return true;
+  }
+
+  Future<Position> getCurrentLocation() async {
+    await ensureLocationPermission();
+    if (!await Geolocator.isLocationServiceEnabled()) {
+      throw Exception('Layanan lokasi tidak aktif');
+    }
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
   }
 }
